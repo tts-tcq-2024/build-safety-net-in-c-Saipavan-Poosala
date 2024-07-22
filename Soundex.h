9bcde7513248bddae845b9377f1da322fddbcc95
Saipavan-Poosala <saipavanpoosala@gmail.com>
2024-07-22 20:51:53 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,49 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
-#include "Soundex.h"
+#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-
+ 
+// Function to get the Soundex code for a character
 char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
+    static const char soundexCodes[26] = {
+        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5',
+        '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
+    };
+    return soundexCodes[toupper(c) - 'A' ];
 }
-
-void generateSoundex(const char *name, char *soundex) {
-    int len = strlen(name);
-    soundex[0] = toupper(name[0]);
-    int sIndex = 1;
-
-    for (int i = 1; i < len && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != soundex[sIndex - 1]) {
-            soundex[sIndex++] = code;
-        }
+ 
+// Function to eliminate zero and repeated values in Soundex code
+char eliminateZeroAndRepeatedValue(char prevcode, char* soundex, size_t i, const char* name) {
+    char code = getSoundexCode(name[i]);
+    if (code != '0' && code != prevcode) {
+        soundex[strlen(soundex)] = code;
+        prevcode = code;
     }
-
-    while (sIndex < 4) {
-        soundex[sIndex++] = '0';
+    return prevcode;
+}
+ 
+// Function to initialize Soundex code with the first character of the name
+void initializeSoundex(char* soundex, char firstChar) {
+    soundex[0] = toupper(firstChar);
+}
+ 
+// Function to pad Soundex code to 4 characters
+void padSoundex(char* soundex) {
+    size_t len = strlen(soundex);
+    while (len < 4) {
+        soundex[len++] = '0';
     }
-
     soundex[4] = '\0';
 }
-
-#endif // SOUNDEX_H
+ 
+// Function to generate Soundex code for a given name
+void generateSoundex(const char* name, char* soundex) {
+    initializeSoundex(soundex, name[0]); // Initialize soundex with the first character of name
+    char prevCode = getSoundexCode(name[0]); // Get Soundex code for the first character
+ 
+    size_t i = 1;
+    while (name[i] != '\0' && i < 4) {
+        prevCode = eliminateZeroAndRepeatedValue(prevCode, soundex, i, name);
+        i++;
+    }
+    padSoundex(soundex);
+}
